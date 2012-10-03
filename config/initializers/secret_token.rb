49d5e9b667631ee2cc63bb41f9795aed2628467b
Marc MacLeod <marbemac@gmail.com>
2012-10-03 15:38:51 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KidSwap::Application.config.secret_token = '9447894d5ff638ff9a5215f3043cfbf0766c90a81e6a3d99c528854dc305a10c51e4ebe25c086371f42601f2f2f1e53339e894dd49ba354c7e3dcd7f6a1f724f'
+KidSwap::Application.config.secret_token = '2a6b8f4b542cf4df116434a7bd83e8dacf7ed7bd28e2f48a945d07c74f4f209732434b9f814d3c23d0c1dd8f73b4bccb8b62d7781ee854169157cee1149bb02d'
