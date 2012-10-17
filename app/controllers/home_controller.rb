ad80b5e602393ee835d8bdcd90d62dc25d1c512a
Marc MacLeod <marbemac@gmail.com>
2012-10-16 20:24:43 -0400

    started layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  layout :determine_layout
+
   def index
-    @users = User.all
+    if signed_in?
+      render "home"
+    else
+      render "splash"
+    end
+  end
+
+  private
+
+  def determine_layout
+    signed_in? ? 'application' : 'splash'
   end
 end
