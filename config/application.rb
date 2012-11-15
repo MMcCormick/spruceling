a376ca95e9165a480c5e331085c70bcd730a124d
Marc MacLeod <marbemac@gmail.com>
2012-11-15 17:08:32 -0500

    asset compiling on heroku

--- config/application.rb
+++ config/application.rb
@@ -70,5 +70,8 @@ module Spruceling
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
