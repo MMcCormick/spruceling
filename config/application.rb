d53eccd78653a54dfe3cbcaa30e74b6b0a456128
Marc MacLeod <marbemac@gmail.com>
2012-11-19 15:01:48 -0500

    precompile admin assets

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,8 @@ module Spruceling
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += ['active_admin.css','active_admin.js']
+
     # for heroku
     config.assets.initialize_on_precompile = false
   end
