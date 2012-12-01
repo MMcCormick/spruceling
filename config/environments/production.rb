a2f425d0630d5af6bf07f0c95dd4fbd35966e022
Marc MacLeod <marbemac@gmail.com>
2012-12-01 13:22:44 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,15 @@ Spruceling::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  # Use a different cache store in production
+  config.cache_store = :dalli_store
+
+  config.action_dispatch.rack_cache = {
+      :metastore    => Dalli::Client.new,
+      :entitystore  => 'file:tmp/cache/rack/body',
+      :allow_reload => false
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
